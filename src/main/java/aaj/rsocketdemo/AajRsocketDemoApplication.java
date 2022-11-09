4a8c7016f18f9c80726656909948bb1233e2d156
Araya2001 <araya2001@outlook.es>
2022-11-08 18:58:55 -0600

    Base project

---
+++ src/main/java/aaj/rsocketdemo/AajRsocketDemoApplication.java
@@ -0,0 +1,17 @@
+package aaj.rsocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@PropertySource("classpath:${PROPERTY_FILE}.properties")
+@EnableAsync
+public class AajRsocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AajRsocketDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aaj/rsocketdemo/AajRsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package aaj.rsocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AajRsocketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
